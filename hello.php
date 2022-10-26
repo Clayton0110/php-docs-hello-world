2ed5a4231407ac85e43e6107f6a5b7e006f11347
Clayton0110 <115100613+Clayton0110@users.noreply.github.com>
2022-10-26 11:51:53 -0500

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC>
+<html lang- "en">
+<head>
+    <meta charset- "UTF-a"
+    <meta http-equiv="X-UA-Compatible" content- "IE-edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1,0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    echo "Hello world";
+    ?>
+
+</body>
+</html>
